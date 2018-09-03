168112bb7eea7a8ef2fea9d8aaedfaf15d152216
Parker Burnett <burnettparker@gmail.com>
2018-09-02 22:55:15 -0600

    Started working on implementing flowerSize option.

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,228 +1,212 @@
-/* This code runs the simulation for the game of s/he loves me s/he loves me not where one would pluck
-petals off a flower to determine if the the admirer of the user truly loves them. */
-
 'use strict';
 
-// Include the Alexa SDK
-console.log("Hello, world!");
+// Import the standard SDK module 
 const Alexa = require('alexa-sdk');
-const languageStrings = require('./languageStrings').languageStrings;
 
-const APP_ID = 'amzn1.ask.skill.eaa51f38-afbb-41ea-a0de-869a81ade996';  //ID for the application
-
-//////////////////////////////////////////////////////////////////////////////
-// Alexa intents
-//////////////////////////////////////////////////////////////////////////////
-
-// The handlers object tells Alexa how to handle various actions
-const handlers = {
-
-  //LaunchRequest is what will run first when the skill is invoked
-  'LaunchRequest' : function() {
-    //The phrase that Alexa will say upon starting the skill
-    this.response.speak(this.t('WELCOME_MSG')).listen(this.t("WELCOME_REPROMPT"));
-    this.emit(':responseReady')
-  },
-
-  //This intent should handle all the requests for guys, I tried picking a manly flower name...
-  'carnationFlowerIntent' : function() {
-    var self = this;
-    /* var gender = this.event.request.intent.slots.maleGender.value;
-    Future error checking I suppose
-    if (true) {
-      this.response.speak("Okay, here we go.");
-    }
-    else {
-      this.response.speak("That isn't a male gender word, try again.");
-    }
-    */
-   var size = Math.floor(Math.random() * (6 - 1) + 1); //make a large flower with petals from 1 to 5
-   console.log("the flower size is: " + size);
-   var theLoveArray = heLoves(size);  //Make theLoveArray filled with strings
-   var finalIndex = size - 1;  //Get the size of the flower and subtract one for size of theLoveArray
-   var finalPhrase = theLoveArray[finalIndex]; //Get the last value of the index in theLoveArray
-   console.log(theLoveArray);
-   console.log("the flower size is NOW: " + size);
-   console.log("the final index is: " + finalIndex);
-   console.log("the final phrase is: " + finalPhrase);
-
-   
-   //TODO: FIX OUTPUT SPEECH
-   theLoveArray = theLoveArray.join(', '); //Add a space in the array for Alexa to sound more natural
-   var initialSpeechOutput = theLoveArray.toString(); //Change the array to a string, Alexa will say the whole Love Array
-
-   if (finalPhrase == "He Loves Me") {
-     var finalSpeachOutputA = initialSpeechOutput + `<break time="1s"/>, Congratulations! He loves you!`;
-     self.response.speak(finalSpeachOutputA);
-    } else {
-      self.response.speak('<amazon:effect name="whispered"> Oooooooooooh...sorry, </amazon:effect>'+`<break time="1s"/>` + finalPhrase );
-    }
-   self.emit(':responseReady');
-  },
-
-  //This is the intent for the girl requests, a more girly flower name...kinda...
-  'daisyFlowerIntent' : function() {
-    var self = this;
-    /* var gender = this.event.request.intent.slots.maleGender.value;
-    Future error checking I suppose
-    if (true) {
-      this.response.speak("Okay, here we go.");
-    }
-    else {
-      this.response.speak("That isn't a male gender word, try again.");
-    }
-    */
-   var size = Math.floor(Math.random() * (6 - 1) + 1); //make a large flower with petals from 1 to 5
-   console.log("the flower size is: " + size);
-   var theLoveArray = sheLoves(size);  //Make theLoveArray filled with strings
-   var finalIndex = size - 1;  //Get the size of the flower and subtract one for size of theLoveArray
-   var finalPhrase = theLoveArray[finalIndex]; //Get the last value of the index in theLoveArray
-   
-   //TODO: FIX OUTPUT SPEECH
-   theLoveArray = theLoveArray.join(', '); //Add a space in the array for Alexa to sound more natural
-   var initialSpeechOutput = theLoveArray.toString(); //Change the array to a string, Alexa will say the whole Love Array
-
-   if (finalPhrase == "She Loves Me") {
-    var finalSpeachOutputA = initialSpeechOutput + `<break time="1s"/>, Congratulations! She loves you!`;
-    self.response.speak(finalSpeachOutputA);
-   } else {
-     self.response.speak('<emphasis level="strong"> Oooooooooooh...sorry, </emphasis>'+`<break time="1s"/>` + finalPhrase);
-   }
-  self.emit(':responseReady');
-   self.emit(':responseReady');
-  },
-
-  'AMAZON.FallbackIntent' : function(){
-    this.response.speak("I don't quite know how to help with that in this skill. Try something else.")
-    this.emit(':responseReady')
-  },
-
-  'AMAZON.CancelIntent': function () {
-    this.response.speak(this.t('SHUTDOWN_MSG'));
-    this.emit('SessionEndedRequest');
-  },
-
-  'AMAZON.HelpIntent' : function() {
-    const speechOutput = this.t('HELP_MSG');
-    const reprompt = this.t('HELP_REPROMPT');
-    this.response.speak(speechOutput).listen(this.t('HELP_REPROMPT'));
-    this.emit(':responseReady');
-  },
-
-  'AMAZON.StopIntent' : function() {
-    this.response.speak(this.t('STOP_MSG'));
-    this.emit(':responseReady');
-  },
-
-  'SessionEndedRequest': function () {
-    this.response.speak(this.t('SHUTDOWN_MSG'));
-    this.emit(':responseReady');
-  },
-
-  'Unhandled': function () {
-    this.attributes.speechOutput = this.t('HELP_MESSAGE');
-    this.attributes.repromptSpeech = this.t('HELP_REPROMPT');
-    this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
-    this.emit(':responseReady');
-  },
-
-}
+const https = require("https");
+// Imports the file with all the messages
+const languageStrings = require('./lambda/custom/languageStrings').languageStrings;
 
 //////////////////////////////////////////////////////////////////////////////
-// Initialize Alexa and connect the application
-//////////////////////////////////////////////////////////////////////////////
-exports.handler = function(event, context) {
-
-// Set up the Alexa object
-	const alexa = Alexa.handler(event, context);
-
-	// configure alexa
-	alexa.appId = APP_ID;
-  //Include the languageStrings library
-	alexa.resources = languageStrings;
-
-	// register alexa function handlers and away we go!
-	alexa.registerHandlers(handlers);
-  // Start the Alexa code
-	alexa.execute();
-}
-
-//////////////////////////////////////////////////////////////////////////////
-// Intent implementation functions
+// Handlers
 //////////////////////////////////////////////////////////////////////////////
 
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+
+        const speakOutput = requestAttributes.t('WELCOME_MSG', requestAttributes.t('SKILL_NAME'));
+        const repromptOutput = requestAttributes.t('WELCOME_REPROMPT');
+
+        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(repromptOutput)
+            .getResponse();
+    },
+};
+
+
+
+// Help the User make informed decisions of how to use the skill
+const HelpHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest' && request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const requestAttributes = attributesManager.getRequestAttributes();
+        return responseBuilder
+            .speak(requestAttributes.t('HELP_MSG'))
+            .reprompt(requestAttributes.t('HELP_MSG'))
+            .getResponse();
+    },
+};
+
+// When the User says stop, quit or cancel (No means no, Alexa!)
+const StopHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+
+        return request.type === 'IntentRequest'
+            && (request.intent.name === 'AMAZON.NoIntent'
+                || request.intent.name === 'AMAZON.CancelIntent'
+                || request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const attributesManager = handlerInput.attributesManager;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        const requestAttributes = attributesManager.getRequestAttributes();
+        return responseBuilder.speak(requestAttributes.t('STOP_MSG')).getResponse();
+    },
+};
+
+
+const FallbackHandler = {
+    // 2018-May-01: AMAZON.FallackIntent is only currently available in en-US locale.
+    // This handler will not be triggered except in that locale, so it can be
+    // safely deployed for any locale.
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest'
+            && request.intent.name === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak(FALLBACK_MESSAGE)
+            .reprompt(FALLBACK_REPROMPT)
+            .getResponse();
+    },
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
+
+        return handlerInput.responseBuilder
+            .speak('Sorry, an error occurred.')
+            .reprompt('Sorry, an error occurred.')
+            .getResponse();
+    },
+};
+
+// Handler for when the user closes the skill
+const SessionEndedHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+        return handlerInput.responseBuilder.getResponse();
+    },
+};
+
+//////////////////////////////////////////////////////////////////////////////
+// Constants and Other Data, Vars, etc...
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+// Helper Functions
+//////////////////////////////////////////////////////////////////////////////
 //Function for the ladies
-function sheLoves(flowerSize){
-  // var smFlower = Math.floor(Math.random() * ((5 - 3 ) + 1) + 3); //make a small flower with petals from 3 to 5
-  // var mdFlower = Math.floor(Math.random() * ((7 - 5) + 1) + 5); //make a medium flower with petals from 5 to 7
-  // var lgFlower = Math.floor(Math.random() * ((9 - 7) + 1) + 7); //make a large flower with petals from 7 to 9
+function sheLoves(flowerSize) {
+    // var smFlower = Math.floor(Math.random() * ((5 - 3 ) + 1) + 3); //make a small flower with petals from 3 to 5
+    // var mdFlower = Math.floor(Math.random() * ((7 - 5) + 1) + 5); //make a medium flower with petals from 5 to 7
+    // var lgFlower = Math.floor(Math.random() * ((9 - 7) + 1) + 7); //make a large flower with petals from 7 to 9
 
-  var lovesMeCounter = 0; //counter to determine how many times to print the phrase "Loves Me" starts at 0
-  var LovesMeNotCounter = 1;  //counter to determine how many times to print the phrase "Loves Me Not" starts at 1
+    var lovesMeCounter = 0; //counter to determine how many times to print the phrase "Loves Me" starts at 0
+    var LovesMeNotCounter = 1;  //counter to determine how many times to print the phrase "Loves Me Not" starts at 1
 
-  var loveArray = []; //Array that will get filled with the phrases to speak
+    var loveArray = []; //Array that will get filled with the phrases to speak
 
-  // this.response.listen('SIZE_PROMPT')  TODO:implementation will come later...
+    // this.response.listen('SIZE_PROMPT')  TODO:implementation will come later...
 
-  //Don't use this here, I will just pass it in to the function
-  // var flowerSize = Math.floor(Math.random() * ((9 - 1) + 1) + 1); //make a large flower with petals from 7 to 9
+    //Don't use this here, I will just pass it in to the function
+    // var flowerSize = Math.floor(Math.random() * ((9 - 1) + 1) + 1); //make a large flower with petals from 7 to 9
 
-  var phraseA = 'She Loves Me'
-  var phraseB = 'She Loves Me Not'
+    var phraseA = 'She Loves Me'
+    var phraseB = 'She Loves Me Not'
 
-  //This will go through for all the even values to add the phrase 'Loves me'
+    //This will go through for all the even values to add the phrase 'Loves me'
     while (lovesMeCounter < flowerSize) {
-      loveArray[lovesMeCounter] = phraseA;
-      lovesMeCounter += 2;
+        loveArray[lovesMeCounter] = phraseA;
+        lovesMeCounter += 2;
     }
 
-  //This will go through for all the even values to add the phrase 'Loves me not'
+    //This will go through for all the even values to add the phrase 'Loves me not'
     while (LovesMeNotCounter < flowerSize) {
-      loveArray[LovesMeNotCounter] = phraseB;
-      LovesMeNotCounter += 2;
+        loveArray[LovesMeNotCounter] = phraseB;
+        LovesMeNotCounter += 2;
     }
     // var theLoveArray = loveArray.toString();  //Change the array of love to a single string
     // const speechOutput = theLoveArray;  //Print the Array
 
     return loveArray;  //Return the loveArray to parse through later.
-
 }
 
 //Function for the fellas
-function heLoves(flowerSize){
-  // var smFlower = Math.floor(Math.random() * ((5 - 3 ) + 1) + 3); //make a small flower with petals from 3 to 5
-  // var mdFlower = Math.floor(Math.random() * ((7 - 5) + 1) + 5); //make a medium flower with petals from 5 to 7
-  // var lgFlower = Math.floor(Math.random() * ((9 - 7) + 1) + 7); //make a large flower with petals from 7 to 9
+function heLoves(flowerSize) {
+    // var smFlower = Math.floor(Math.random() * ((5 - 3 ) + 1) + 3); //make a small flower with petals from 3 to 5
+    // var mdFlower = Math.floor(Math.random() * ((7 - 5) + 1) + 5); //make a medium flower with petals from 5 to 7
+    // var lgFlower = Math.floor(Math.random() * ((9 - 7) + 1) + 7); //make a large flower with petals from 7 to 9
 
-  var lovesMeCounter = 0; //counter to determine how many times to print the phrase "Loves Me" starts at 0
-  var LovesMeNotCounter = 1;  //counter to determine how many times to print the phrase "Loves Me Not" starts at 1
+    var lovesMeCounter = 0; //counter to determine how many times to print the phrase "Loves Me" starts at 0
+    var LovesMeNotCounter = 1;  //counter to determine how many times to print the phrase "Loves Me Not" starts at 1
 
-  var loveArray = []; //Array that will get filled with the phrases to speak
+    var loveArray = []; //Array that will get filled with the phrases to speak
 
-  // this.response.listen('SIZE_PROMPT')  TODO:implementation will come later...
+    // this.response.listen('SIZE_PROMPT')  TODO:implementation will come later...
 
-  var phraseA = 'He Loves Me'
-  var phraseB = 'He Loves Me Not'
+    var phraseA = 'He Loves Me'
+    var phraseB = 'He Loves Me Not'
 
-  //This will go through for all the even values to add the phrase 'Loves me'
+    //This will go through for all the even values to add the phrase 'Loves me'
     while (lovesMeCounter < flowerSize) {
-      loveArray[lovesMeCounter] = phraseA;
-      lovesMeCounter += 2;
+        loveArray[lovesMeCounter] = phraseA;
+        lovesMeCounter += 2;
     }
 
-  //This will go through for all the even values to add the phrase 'Loves me not'
+    //This will go through for all the even values to add the phrase 'Loves me not'
     while (LovesMeNotCounter < flowerSize) {
-      loveArray[LovesMeNotCounter] = phraseB;
-      LovesMeNotCounter += 2;
+        loveArray[LovesMeNotCounter] = phraseB;
+        LovesMeNotCounter += 2;
     }
     // var theLoveArray = loveArray.toString();  //Change the array of love to a single string
     // const speechOutput = theLoveArray;  //Print the Array
 
     return loveArray;  //Return the loveArray to parse through later.
-
 }
 
-//Shutdown the skill with speech
-function closeWithSpeech(self) {
-	self.emit(':tell', self.t('SHUTDOWN_MSG'));
-}
+//////////////////////////////////////////////////////////////////////////////
+// Export LAMBDA SETUP
+//////////////////////////////////////////////////////////////////////////////
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        HelpHandler,
+        StopHandler,
+        FallbackHandler,
+        ErrorHandler,
+        SessionEndedRequestHandler
+    )
+    .addRequestInterceptors(LocalizationInterceptor)
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
--- lovesMe.js
+++ lovesMe.js
@@ -1,9 +1,13 @@
 ///////////////////////////////////////////////////////////////////
 //Static Variables
 //////////////////////////////////////////////////////////////////
-var smFlower = Math.floor(Math.random() * ((5 - 3 ) + 1) + 3); //make a small flower with petals from 3 to 5
-var mdFlower = Math.floor(Math.random() * ((7 - 5) + 1) + 5); //make a medium flower with petals from 5 to 7
-var lgFlower = Math.floor(Math.random() * ((9 - 7) + 1) + 7); //make a large flower with petals from 7 to 9
+var smFlower = Math.floor(Math.random() * 3) + 3; //make a small flower with petals from 3 to 5
+var mdFlower = Math.floor(Math.random() * 3) + 5; //make a medium flower with petals from 5 to 7
+var lgFlower = Math.floor(Math.random() * 3) + 7; //make a large flower with petals from 7 to 9
+
+console.log(`Small Flower Size: %s`, smFlower);
+console.log(`Medium Flower Size: %s`, mdFlower);
+console.log(`Large Flower Size: %s \n`, lgFlower);
 
 var lovesMeCounter = 0; //counter to determine how many times to print the phrase "Loves Me" starts at 0
 var LovesMeNotCounter = 1;  //counter to determine how many times to print the phrase "Loves Me Not" starts at 1
@@ -13,20 +17,11 @@ var loveArray = []; //Array that will get filled with the phrases to speak
 var phraseA = 'Loves Me'
 var phraseB = 'Loves Me Not'
 
+var size_of_flowers = ['Small', 'Medium', 'Large'];    
+
 //////////////////////////////////////////////////////////////////////////////
 // Intent implementation functions
 //////////////////////////////////////////////////////////////////////////////
-
-//TODO: Not really using this function atm...Probably should just ditch it
-//Function to determine if the flower number is even or odd
-function isEven(number) {
-  if (number % 2 == 0) {
-    return true;  //Number is even
-  }
-  else {
-    return false; //Number is not even
-  }
-}
 
 //Function that will make and fill the array depending on the flower size
 function makeFlower(flowerSize){
@@ -43,34 +38,6 @@ function makeFlower(flowerSize){
   }
   return loveArray; //Return the filled array.
 }
-
-//TEST 1: Test generating flower sizes and what values are returned
-
-//Small flower test
-// console.log("Running Tests on small flower...", '\n')
-// console.log("The size of the small flower is:", smFlower);
-// makeFlower(smFlower);
-// console.log("The first value of the array is: " + loveArray[0]);
-// console.log("Size of the array:", loveArray.length);
-// console.log("The full array is:", loveArray.toString(), '\n')
-
-//Medium Flower test
-// console.log("Running Tests on medium flower...", '\n')
-// console.log("The Size of the medium flower is:", mdFlower);
-// makeFlower(mdFlower);
-// console.log("The first value of the array is: " + loveArray[0]);
-// console.log("Size of the array:", loveArray.length);
-// console.log("The full array is:", loveArray.toString(), '\n')
-
-//Large Flower Test
-// console.log("Running Tests on large flower...", '\n')
-// console.log("The Size of the large flower is:", lgFlower);
-// makeFlower(lgFlower);
-// console.log("The first value of the array is: " + loveArray[0]);
-// console.log("Size of the array:", loveArray.length);
-// console.log("The full array is:", loveArray.toString(), '\n')
-
-// END TEST 1
 
 // TEST 2: Tests getting the final value which I will store in order for alexa to be able to
 // definitively say the results
@@ -100,21 +67,58 @@ function makeFlower(flowerSize){
 // END TEST 2
 
 // TEST 3
-//Function for the ladies
+//Function for the men
+// function heLoves(flowerSize){
+
+//   //This will go through for all the even values to add the phrase 'Loves me'
+//     while (lovesMeCounter < flowerSize) {
+//       loveArray[lovesMeCounter] = phraseA;
+//       lovesMeCounter += 2;
+//     }
+
+//   //This will go through for all the even values to add the phrase 'Loves me not'
+//     while (LovesMeNotCounter < flowerSize) {
+//       loveArray[LovesMeNotCounter] = phraseB;
+//       LovesMeNotCounter += 2;
+//     }
+//     // var theLoveArray = loveArray.toString();  //Change the array of love to a single string
+//     // const speechOutput = theLoveArray;  //Print the Array
+
+//     return loveArray;  //Return the loveArray to parse through later.
+
+// }
+// console.log("STARTING TEST #3\n");
+
+// var size = Math.floor(Math.random() * (6 - 1) + 1);
+// console.log("The flower is size: " + size);
+
+// var theLoveArray = heLoves(size);  //Make theLoveArray filled with strings
+// var finalIndex = size - 1;  //Get the size of the flower and subtract one for size of theLoveArray
+// var finalPhrase = theLoveArray[finalIndex]; //Get the last value of the index in theLoveArray
+
+// theLoveArray = theLoveArray.join(', '); // Join adds a comma and space delimiter between each string, hopefully smoothing dialogue
+// var newLoveArray = theLoveArray.toString();
+
+// console.log("Final index " + finalIndex);
+// console.log("The array is: " + newLoveArray);
+
+// if (finalPhrase == "He Loves Me") {
+//   console.log("Congrats, " + finalPhrase)
+// } else {
+//   console.log("Ooooh...sorry " + finalPhrase)
+// }
+
+// console.log("ENDING TEST #3\n");
+//END
+
+// Test #4 will be for figuring out how to implement the size feature.
+
+var randomFlowerSize = size_of_flowers[Math.floor(Math.random() * size_of_flowers.length)];
+
+//Test to see the randomness of the flower size
+console.log(`The -RANDOM- flower size is: %s \n`, randomFlowerSize);
+
 function heLoves(flowerSize){
-  // var smFlower = Math.floor(Math.random() * ((5 - 3 ) + 1) + 3); //make a small flower with petals from 3 to 5
-  // var mdFlower = Math.floor(Math.random() * ((7 - 5) + 1) + 5); //make a medium flower with petals from 5 to 7
-  // var lgFlower = Math.floor(Math.random() * ((9 - 7) + 1) + 7); //make a large flower with petals from 7 to 9
-
-  var lovesMeCounter = 0; //counter to determine how many times to print the phrase "Loves Me" starts at 0
-  var LovesMeNotCounter = 1;  //counter to determine how many times to print the phrase "Loves Me Not" starts at 1
-
-  var loveArray = []; //Array that will get filled with the phrases to speak
-
-  // this.response.listen('SIZE_PROMPT')  TODO:implementation will come later...
-
-  var phraseA = 'He Loves Me'
-  var phraseB = 'He Loves Me Not'
 
   //This will go through for all the even values to add the phrase 'Loves me'
     while (lovesMeCounter < flowerSize) {
@@ -133,9 +137,19 @@ function heLoves(flowerSize){
     return loveArray;  //Return the loveArray to parse through later.
 
 }
-console.log("STARTING TEST #3\n");
+console.log("READY?\n");
+console.log("FIGHT!\n");
 
-var size = Math.floor(Math.random() * (6 - 1) + 1);
+if (randomFlowerSize === "Large") {
+  size = lgFlower;
+} else if (randomFlowerSize === "Medium") {
+  size = mdFlower;
+} else if (randomFlowerSize === "Small") {
+  size = smFlower;
+} else {
+  size = 1; //Don't really know what else to set it to...mainly for error checking I guess.
+}
+
 console.log("The flower is size: " + size);
 
 var theLoveArray = heLoves(size);  //Make theLoveArray filled with strings
@@ -148,11 +162,11 @@ var newLoveArray = theLoveArray.toString();
 console.log("Final index " + finalIndex);
 console.log("The array is: " + newLoveArray);
 
-if (finalPhrase == "He Loves Me") {
+if (finalPhrase == "Loves Me") {
   console.log("Congrats, " + finalPhrase)
 } else {
   console.log("Ooooh...sorry " + finalPhrase)
 }
 
-console.log("ENDING TEST #3\n");
-//END
+console.log("ENDING TEST #4\n");
+// End Test #4
